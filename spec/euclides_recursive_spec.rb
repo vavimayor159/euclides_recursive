85f4511463ad0287ca16fbbdfa784d8c072bb05a
Ivan Garcia <ivandevp@gmail.com>
2018-12-18 23:37:56 -0600

    Add a new test

--- spec/euclides_recursive_spec.rb
+++ spec/euclides_recursive_spec.rb
@@ -18,4 +18,9 @@ RSpec.describe EuclidesRecursive do
   it "calculates the right mcd of 1357 and 225" do
     expect(test_object.mcd(1357, 225)).to be(1)
   end
+
+  it "mcd is a reflexive operation" do
+    expect(test_object.mcd(865, 1045)).to be(5)
+    expect(test_object.mcd(1045, 865)).to be(5)
+  end
 end
